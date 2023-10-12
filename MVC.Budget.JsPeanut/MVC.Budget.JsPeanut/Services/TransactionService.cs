d560edb0d469ba7f68f0266974cd0571fa5b1c5d
JsPeanut <cacahuatearg223@outlook.com>
2023-10-12 14:47:38 -0300

    Added modals

---
+++ MVC.Budget.JsPeanut/MVC.Budget.JsPeanut/Services/TransactionService.cs
@@ -0,0 +1,25 @@
+﻿using MVC.Budget.JsPeanut.Data;
+using MVC.Budget.JsPeanut.Models;
+using System.Transactions;
+
+namespace MVC.Budget.JsPeanut.Services
+{
+    public class TransactionService
+    {
+        private readonly DataContext _context;
+        public TransactionService(DataContext context) 
+        {
+            _context = context;
+        }
+        public List<Models.Transaction> GetAll()
+        {
+            var transactions = _context.Transactions.ToList();
+
+            return transactions;
+        }
+        public void Add()
+        {
+
+        }
+    }
+}
