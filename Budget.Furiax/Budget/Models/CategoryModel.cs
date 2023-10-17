117498598df0915bd6f1ee5cdbe9ff2bea3ec4f1
furiax <carlmalfliet@proximus.be>
2023-10-17 20:16:49 +0200

    Creating the models

---
+++ Budget.Furiax/Budget/Models/CategoryModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Budget.Models
+{
+    public class CategoryModel
+    {
+        public int CategoryId { get; set; }
+        public string? CategoryName { get; set; }
+
+        public ICollection<TransactionModel> Transactions { get; set; }
+    }
+}
---
+++ Budget.Furiax/Budget/Models/TransactionModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Budget.Models
+{
+    public class TransactionModel
+    {
+        public int TransactionId { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public string? TransactionSource { get; set; }
+        public decimal TransactionAmount { get; set; }
+
+        public CategoryModel Category { get; set; }
+    }
+}
