5c27e8d61812c4e069573e95164e3d8931e08e9f
furiax <carlmalfliet@proximus.be>
2023-11-14 10:06:42 +0100

    scafolded the controllers

---
+++ Budget.Furiax/Budget/Controllers/CategoryController.cs
@@ -0,0 +1,119 @@
+﻿using Budget.DataAccess;
+using Budget.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Budget.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly BudgetContext _context;
+
+        public CategoryController(BudgetContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Category
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategoryModel>>> GetCategories()
+        {
+            if (_context.Categories == null)
+            {
+                return NotFound();
+            }
+            return await _context.Categories.ToListAsync();
+        }
+
+        // GET: api/Category/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CategoryModel>> GetCategoryModel(int id)
+        {
+            if (_context.Categories == null)
+            {
+                return NotFound();
+            }
+            var categoryModel = await _context.Categories.FindAsync(id);
+
+            if (categoryModel == null)
+            {
+                return NotFound();
+            }
+
+            return categoryModel;
+        }
+
+        // PUT: api/Category/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCategoryModel(int id, CategoryModel categoryModel)
+        {
+            if (id != categoryModel.CategoryId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(categoryModel).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CategoryModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Category
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<CategoryModel>> PostCategoryModel(CategoryModel categoryModel)
+        {
+            if (_context.Categories == null)
+            {
+                return Problem("Entity set 'BudgetContext.Categories'  is null.");
+            }
+            _context.Categories.Add(categoryModel);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCategoryModel", new { id = categoryModel.CategoryId }, categoryModel);
+        }
+
+        // DELETE: api/Category/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCategoryModel(int id)
+        {
+            if (_context.Categories == null)
+            {
+                return NotFound();
+            }
+            var categoryModel = await _context.Categories.FindAsync(id);
+            if (categoryModel == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categories.Remove(categoryModel);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CategoryModelExists(int id)
+        {
+            return (_context.Categories?.Any(e => e.CategoryId == id)).GetValueOrDefault();
+        }
+    }
+}
