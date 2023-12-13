e9ec07d34f31a6e9bd46a9e55b559743a0cf01fc
furiax <carlmalfliet@proximus.be>
2023-12-13 13:45:28 +0100

    implemented delete transaction

--- Budget.Furiax/Budget/wwwroot/js/site.js
+++ Budget.Furiax/Budget/wwwroot/js/site.js
@@ -202,7 +202,6 @@ function deleteCategory() {
         console.log("user clicked cancel");
     }
 }
-
 function displayTransactions(data) {
     const tList = document.getElementById('transactionList');
     const tBody = tList.querySelector('tBody');
@@ -263,7 +262,7 @@ function displayTransactions(data) {
             <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
         </svg>`;
         deleteTransactionButton.addEventListener('click', () => {
-            openDeleteTransactionModal();
+            deleteTransaction(item.transactionId);
         });
         td7.appendChild(deleteTransactionButton);
     });
@@ -317,7 +316,6 @@ function editTransaction() {
         Category: selectedEditCategory,
         CategoryId: selectedEditCategory.CategoryId
     }
-    console.log(editedItem);
 
     fetch(uriTransaction + "/" + editTransactionId, {
         method: 'PUT',
@@ -343,8 +341,26 @@ function editTransaction() {
         })
         .catch(error => console.error('Could not update the transaction', error.message));
 }
-function openDeleteTransactionModal() {
-    console.log('opens up delete transaction modal');
+function deleteTransaction(transactionId) {
+    const askForConfirmation = confirm("Delete transaction with id " + transactionId + " ?");
+    if (askForConfirmation) {
+        fetch(uriTransaction + "/" + transactionId, {
+            method: 'DELETE',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => {
+             if (!response.ok) {
+                 throw new Error(`HTTP error! Status: ${response.status}`);
+             }
+            location.reload();
+        })
+        .catch (error => {
+            console.error('An error occured while deleting transaction: ', error.message);
+        });
+    }
 }
 function convertDate(dateString) {
     const date = new Date(dateString);
