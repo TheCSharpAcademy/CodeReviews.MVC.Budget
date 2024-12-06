e0cfb5f7f1430fc7079e2a1caca4e0ed2fd822fa
hasona23 <130175104+hasona23@users.noreply.github.com>
2024-12-06 15:24:40 +0200

    Add Models

---
+++ Budget.hasona23/Budget.hasona23/Models/Category.cs
@@ -0,0 +1,7 @@
+﻿namespace Budget.hasona23.Models;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+}
---
+++ Budget.hasona23/Budget.hasona23/Models/Transaction.cs
@@ -0,0 +1,11 @@
+﻿namespace Budget.hasona23.Models;
+
+public class Transaction
+{
+    public int Id { get; set; }
+    public string Category { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public float Price { get; set; }
+    public bool IsProfit { get; set; }
+    public DateTime Date { get; set; }
+}
