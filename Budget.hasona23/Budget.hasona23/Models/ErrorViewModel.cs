e3e269212c605599b40cca6e05452783fa9b74db
hasona23 <130175104+hasona23@users.noreply.github.com>
2024-12-06 14:35:25 +0200

    First Commit

---
+++ Budget.hasona23/Budget.hasona23/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Budget.hasona23.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
